e0ef3ef9417031f11eeaf3de49567a6cbab33a1b
Savannah C <sgcorrell@gmail.com>
2019-06-20 15:01:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,8 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    if @user != nil && 
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
   end
 
   get '/account' do
